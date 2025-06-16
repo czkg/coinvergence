5d0be5591d196007bbe83f99b99f886d611d6b0a
cz <czdutss@gmail.com>
2025-06-15 23:55:43 -0400

    6/15/25

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,6 +1,5 @@
 import React, { useEffect } from "react"
 import ReactDOM from "react-dom/client"
-//import './index.css'
 import RouterWrapper from "./RouterWrapper"
 
 
--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -4,6 +4,8 @@ import path from "path"
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+  ],
   base: "/",
 })
